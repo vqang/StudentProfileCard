d7c55e3421e333a0ad32a15f21d8ed03ebc70caf
vqang <quang21042006@gmail.com>
2026-09-17 21:29:38 +0700

    Hoàn thành Bước 4

--- app/src/main/java/com/ute/studentprofile/MainActivity.kt
+++ app/src/main/java/com/ute/studentprofile/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.ute.studentprofile
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ute.studentprofile.databinding.ActivityMainBinding
-
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var currentStudent = Student(
@@ -18,25 +16,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         bindStudentData(currentStudent)
-        binding.btnUpdateGpa.setOnClickListener {
-            val inputStr = binding.edtNewGpa.text.toString().trim()
-            val newGpa = inputStr.toDoubleOrNull()
-            if (newGpa == null || newGpa !in 0.0..4.0) {
-                binding.edtNewGpa.error = "Vui lòng nhập GPA hợp lệ (0.0 - 4.0)"
-                toast("Điểm GPA không hợp lệ!")
-                return@setOnClickListener
-            }
-            currentStudent = currentStudent.copy(gpa = newGpa)
-            bindStudentData(currentStudent)
-            toast("Cập nhật điểm thành công!")
-        }
     }
     private fun bindStudentData(student: Student) {
         with(binding) {
             tvName.text = student.name
-            tvStudentId.text = "MSSV: ${student.id} Lớp: ${student.className}"
+            tvStudentId.text = "MSSV: ${student.id} • Lớp: ${student.className}"
             tvGpaBadge.text = "${student.gpa} GPA (${student.gpa.toAcademicRanking()})"
-            edtNewGpa.setText("")
+            edtNewGpa.setText(student.gpa.toString())
         }
     }
 }
