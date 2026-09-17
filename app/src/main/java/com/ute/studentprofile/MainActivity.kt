676d1340300908b16040a58c7dd6be7d39258596
vqang <quang21042006@gmail.com>
2026-09-17 21:56:57 +0700

    Hoàn thành Bước 5

--- app/src/main/java/com/ute/studentprofile/MainActivity.kt
+++ app/src/main/java/com/ute/studentprofile/MainActivity.kt
@@ -16,6 +16,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         bindStudentData(currentStudent)
+        binding.btnUpdateGpa.setOnClickListener {
+            val inputStr = binding.edtNewGpa.text.toString().trim()
+            val newGpa = inputStr.toDoubleOrNull()
+            if (newGpa == null || newGpa !in 0.0..4.0) {
+                binding.edtNewGpa.error = "Vui lòng nhập GPA hợp lệ (0.0 - 4.0)"
+                toast("Điểm GPA không hợp lệ!")
+                return@setOnClickListener
+            }
+            currentStudent = currentStudent.copy(gpa = newGpa)
+            bindStudentData(currentStudent)
+            toast("Cập nhật điểm thành công!")
+        }
     }
     private fun bindStudentData(student: Student) {
         with(binding) {
